daecd8680123cf9665e8731d39753a305a10e151
César Ferreira <cesarferreiraa67@gmail.com>
2021-08-07 22:17:53 -0300

    Template admin

---
+++ backend/views/layouts/left.php
@@ -0,0 +1,54 @@
+<?php
+use yii\bootstrap\Nav;
+
+?>
+<aside class="main-sidebar ">
+
+    <section class="sidebar ">
+
+        <!-- Sidebar user panel -->
+        <div class="user-panel">
+            <div class="pull-left image">
+                <img src="<?= $directoryAsset ?>/img/user2-160x160.jpg" class="img-circle" alt="User Image"/>
+            </div>
+            <div class="pull-left info">
+                <p>Alexander Pierce</p>
+
+                <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+            </div>
+        </div>
+
+        <!-- search form -->
+        <form action="#" method="get" class="sidebar-form">
+            <div class="input-group">
+                <input type="text" name="q" class="form-control" placeholder="Search..."/>
+              <span class="input-group-btn">
+                <button type='submit' name='search' id='search-btn' class="btn btn-flat"><i class="fa fa-search"></i>
+                </button>
+              </span>
+            </div>
+        </form>
+        <!-- /.search form -->
+
+        <?=
+        Nav::widget(
+            [
+                'encodeLabels' => false,
+                'options' => ['class' => 'sidebar-menu'],
+                'items' => [
+                    '<li class="header">Menu Yii2</li>',
+                    ['label' => '<i class="fa fa-file-code-o"></i><span>Agendamentos</span>', 'url' => ['/agendamentos']],
+                    ['label' => '<i class="fa fa-dashboard"></i><span>Pacientes</span>', 'url' => ['/pacientes']],
+                    ['label' => '<i class="fa fa-dashboard"></i><span>Exames</span>', 'url' => ['/exames']],
+                    ['label' => '<i class="fa fa-dashboard"></i><span>Profissionais</span>', 'url' => ['/debug']],
+                    ['label' => '<i class="fa fa-dashboard"></i><span>Resultados exames</span>', 'url' => ['/debug']],
+                    
+                ],
+            ]
+        );
+        ?>
+
+       
+    </section>
+
+</aside>
